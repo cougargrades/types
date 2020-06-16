c77566f390e7f4b8ade7b7f665028684debd27e1
Austin Jackson <themacphage@gmail.com>
2020-06-16 18:33:11 -0500

    i still dont know what im doing

--- index.d.ts
+++ index.d.ts
@@ -24,3 +24,12 @@ export default class TokenMetadata {
   serialize(): any;
 }
 
+/**
+ * Object representation of `edu.uh.grade_distribution`.
+ * See: https://github.com/cougargrades/publicdata/tree/master/documents/edu.uh.grade_distribution
+ */
+export declare namespace EduUHGradeDistribution {
+  export interface CSVRow {
+    TERM: string;
+  }
+}
--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="typescript" />
-
-export { default as Token } from './Token';
-export { TokenPermissions } from './Token';
-
-export { EduUHGradeDistribution } from './EduUHGradeDistribution';
