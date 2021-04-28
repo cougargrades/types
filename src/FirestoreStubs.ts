9e9c9ec7719516d855e883add3e1def60b8d6895
Austin Jackson <themacphage@gmail.com>
2021-04-27 22:49:59 -0500

    removed native FieldValue dependencies and deprecated stub

--- src/FirestoreStubs.ts
+++ src/FirestoreStubs.ts
@@ -288,13 +288,73 @@ export type Transaction = {
   delete(documentRef: DocumentReference<any>): Transaction;
 }
 
-// Acquire native FieldValue types
-import { default as firebase } from 'firebase';
-import FrontFieldValue = firebase.firestore.FieldValue;
-import * as admin from 'firebase-admin';
-import AdminFieldValue = admin.firestore.FieldValue;
+/**
+ * Sentinel values that can be used when writing document fields with set(),
+ * create() or update().
+ * @deprecated
+ */
+export type FieldValue = Partial<{
 
-// Export the things
-export type FieldValue = FrontFieldValue | AdminFieldValue;
-export { FrontFieldValue, AdminFieldValue };
-export { isNode } from 'browser-or-node';
+  /**
+   * Returns a sentinel used with set(), create() or update() to include a
+   * server-generated timestamp in the written data.
+   *
+   * @return The FieldValue sentinel for use in a call to set(), create() or
+   * update().
+   */
+  serverTimestamp(): FieldValue;
+
+  /**
+   * Returns a sentinel for use with update() or set() with {merge:true} to
+   * mark a field for deletion.
+   *
+   * @return The FieldValue sentinel for use in a call to set() or update().
+   */
+  delete(): FieldValue;
+
+  /**
+   * Returns a special value that can be used with set(), create() or update()
+   * that tells the server to increment the field's current value by the given
+   * value.
+   *
+   * If either current field value or the operand uses floating point
+   * precision, both values will be interpreted as floating point numbers and
+   * all arithmetic will follow IEEE 754 semantics. Otherwise, integer
+   * precision is kept and the result is capped between -2^63 and 2^63-1.
+   *
+   * If the current field value is not of type 'number', or if the field does
+   * not yet exist, the transformation will set the field to the given value.
+   *
+   * @param n The value to increment by.
+   * @return The FieldValue sentinel for use in a call to set(), create() or
+   * update().
+   */
+  increment(n: number): FieldValue;
+
+  /**
+   * Returns a special value that can be used with set(), create() or update()
+   * that tells the server to union the given elements with any array value
+   * that already exists on the server. Each specified element that doesn't
+   * already exist in the array will be added to the end. If the field being
+   * modified is not already an array it will be overwritten with an array
+   * containing exactly the specified elements.
+   *
+   * @param elements The elements to union into the array.
+   * @return The FieldValue sentinel for use in a call to set(), create() or
+   * update().
+   */
+  arrayUnion(...elements: any[]): FieldValue;
+
+  /**
+   * Returns a special value that can be used with set(), create() or update()
+   * that tells the server to remove the given elements from any array value
+   * that already exists on the server. All instances of each element
+   * specified will be removed from the array. If the field being modified is
+   * not already an array it will be overwritten with an empty array.
+   *
+   * @param elements The elements to remove from the array.
+   * @return The FieldValue sentinel for use in a call to set(), create() or
+   * update().
+   */
+  arrayRemove(...elements: any[]): FieldValue;
+}>;
